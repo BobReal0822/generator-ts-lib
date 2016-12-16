7e1b841e49147328aabcbedbd7f76ece56122594
易乙 <yangyang.mayangyan@alibaba-inc.com>
2016-12-16 15:20:02 +0800

    feat: basically finish

---
+++ generators/app/index.js
@@ -0,0 +1,46 @@
+/**
+ * generator-ts-lib 
+ */
+
+var path = require('path'),
+    chalk = require('chalk'),
+    yeoman = require('yeoman-generator'),
+    yosay = require('yosay');
+
+var Reactpackage = yeoman.Base.extend({
+    info: function() {
+        this.log(chalk.green(
+            'I am going to build your app!'
+        ));
+    },
+
+    generateBasic: function() {
+        this.directory('src', 'src');
+        this.directory('build', 'build');
+        this.directory('test', 'test');
+        this.directory('example', 'example');
+        this.copy('package.json', 'package.json');
+        this.copy('.gitignore', '.gitignore');
+        this.copy('.npmignore', '.mpmignore');
+        this.copy('README.md', 'README.md');
+    },
+
+    generateClient: function() {
+        this.sourceRoot(path.join(__dirname, 'templates'));
+        this.destinationPath('./');
+    },
+
+    install: function() {      //安装依赖
+        this.installDependencies({
+            skipInstall: this.options['skip-install']
+        });
+    },
+
+    end: function() {
+        this.log(yosay(
+            'Your app has been created successfully!'
+        ));
+    }
+});
+
+module.exports = Reactpackage;
