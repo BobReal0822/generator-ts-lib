31eaeb24dbd68c7075437f7cd9b8b053af289658
易乙 <yangyang.mayangyan@alibaba-inc.com>
2017-01-03 14:36:56 +0800

    Optimize

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,7 +10,7 @@ var path = require('path'),
 var TsLibPackage = yeoman.Base.extend({
     info: function() {
         this.log(chalk.green(
-            'I am going to build your app!'
+            'Generating typescript library now: '
         ));
     },
 
